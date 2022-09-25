c0cc0f9913f05fd380c151e153dbe2c0fe63a938
Raven498 <78182016+Raven498@users.noreply.github.com>
2022-09-24 21:01:02 -0400

    adding current code

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Town{
+  protected:
+    string name;
+    int population;
+    int money;
+    int shops;
+    int econ_rating;
+    int joy;
+
+  public:
+    string getName(){return name;}
+    int getPop(){return population;}
+    int getMoney(){return money;}
+    int getShops(){return shops;}
+    int getEcon(){return econ_rating;}
+    int getJoy(){return joy;}
+
+    string setName(string name)
+    
+};
+
+class City{
+
+};
+
+class Gov{
+
+};
+
+class Health{
+
+};
+
+class Defense{
+
+};
+
+class Operation{
+
+};
+
+class Law{
+
+};
+int main() {
+  
+}
